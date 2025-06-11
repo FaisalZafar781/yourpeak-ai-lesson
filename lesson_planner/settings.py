cb11be20d99975879310090bb84f72b23bfb6de6
hamzayaqoob1509 <hamzayaqoob879@gmail.com>
2025-06-11 19:17:42 +0500

    smtp creds updated

--- lesson_planner/settings.py
+++ lesson_planner/settings.py
@@ -169,5 +169,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ' faisalzafar781@gmail.com'
-EMAIL_HOST_PASSWORD = 'rckn vdrc iwlp wxeb'
+EMAIL_HOST_USER = 'mikelies@businessworksolutions.com'
+EMAIL_HOST_PASSWORD = 'xtyw kjei euie fslh'
